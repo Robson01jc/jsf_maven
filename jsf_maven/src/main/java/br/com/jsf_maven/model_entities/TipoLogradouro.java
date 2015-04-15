2d8c8d358058a73c5f2ce9edab28a30648ba707d
Robson <Robson@DGTI-366260RE>
2015-04-15 17:49:09 -0300

    Adição das Entidades relacionadas a Endereco.

---
+++ jsf_maven/src/main/java/br/com/jsf_maven/model_entities/TipoLogradouro.java
@@ -0,0 +1,80 @@
+package br.com.jsf_maven.model_entities;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.ForeignKey;
+
+@Entity
+@Table(name = "tipoLogradouro")
+public class TipoLogradouro implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "IdTipoLogradouro", nullable = false)
+    private Integer idTipoLogradouro;
+    @Column(name = "DescricaoTipoLogradouro", nullable = false, length = 35)
+    private String descricaoTipoLogradouro;
+    
+    @OneToMany(mappedBy = "tipoLogradouro", fetch = FetchType.LAZY)
+    @ForeignKey(name = "TipoLogradouro_Endereco")
+    private List<Endereco> enderecos;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 43 * hash + Objects.hashCode(this.idTipoLogradouro);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TipoLogradouro other = (TipoLogradouro) obj;
+        if (!Objects.equals(this.idTipoLogradouro, other.idTipoLogradouro)) {
+            return false;
+        }
+        return true;
+    }
+
+    public Integer getIdTipoLogradouro() {
+        return idTipoLogradouro;
+    }
+
+    public void setIdTipoLogradouro(Integer idTipoLogradouro) {
+        this.idTipoLogradouro = idTipoLogradouro;
+    }
+
+    public String getDescricaoTipoLogradouro() {
+        return descricaoTipoLogradouro;
+    }
+
+    public void setDescricaoTipoLogradouro(String descricaoTipoLogradouro) {
+        this.descricaoTipoLogradouro = descricaoTipoLogradouro;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public TipoLogradouro() {
+    }
+}
